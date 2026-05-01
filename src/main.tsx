bb961e70ed7a5acdb9022c0160b81570a46453e1
dailyraiser <167914669+dailyraiser@users.noreply.github.com>
2026-05-01 11:28:26 -0400

    feat: CSS custom property for runtime stone texture override with localStorage persistence

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const storedTexture = localStorage.getItem('prayer-wall:stone-texture')
+if (storedTexture) {
+  document.documentElement.style.setProperty('--stone-texture-url', `url(${storedTexture})`)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
